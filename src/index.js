fa894214134ecc393dd791cfc21abd1122ab1617
Tushar Agarwal <tushar7436@gmail.com>
2025-09-11 17:48:42 +0530

    Created Nodemailer Transport Object

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ const express = require('express');
 const { ServerConfig, Logger } = require('./config');
 const apiRoutes = require('./routes');
 
+const mailsender = require('./config/email-config');
 
 const app = express();
 
+app.use(express.json());   
+app.use(express.urlencoded({extended: true}));
+
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`sucessfully started the server on PORT: ${ServerConfig.PORT}`);
     Logger.info("Sucessfully started the server");
+    try{
+            const response = await mailsender.sendMail({
+            from: ServerConfig.GMAIL_EMAIL,
+            to: 'tusharagarwal2022@vitbhopal.ac.in',
+            subject: 'Is the service working ?',
+            content: 'Yes It is wotking'
+        });
+        console.log(response)
+    } catch(error){
+        console.log(error);
+    }
 });
---
+++ src/utils/common/enums.js
@@ -0,0 +1,9 @@
+NOTI_TYPES_ENUMS = {
+    PENDING: 'pending',
+    SUCCESS: 'success',
+    FAILED: 'failed'
+}
+
+module.exports = {
+    NOTI_TYPES_ENUMS
+}
